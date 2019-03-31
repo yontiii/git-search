ed1b5255040d515a331ec89c258c7db1339e44a2
yontey <muasajohn01@gmail.com>
2019-03-31 14:49:28 +0300

    added profiles class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
@@ -12,7 +12,8 @@ import { UsersComponent } from './users/users.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Profile} from '../profile';
+import {HttpClient} from '@angular/common/http'
 
 @Component({
   selector: 'app-users',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+    profile:Profile;
+    username:string;
+  constructor(private http:HttpClient) { }
 
   ngOnInit() {
+      interface profileResponse{
+         login:string,
+         name:string,
+         avatar_url:string,
+        followers:string,
+        following:string,
+        html_url:string,
+        company:string,
+        location:string,
+        bio:string,
+        public_repos:string,
+      }
+
+    this.username="yontiii"
+    this.http.get<profileResponse>("https://api.github.com/users/yontiii?access_token=45da1acab3858d966747dcff67db37fbfcd81bae ").subscribe(data=>{
+      this.profile= new Profile(data.login,data.login,data.avatar_url,
+        data.followers,data.following,data.html_url,data.company,data.location,data.bio,data.public_repos)
+    })
+      
   }
 
 }
