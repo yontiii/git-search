56466af7aae6f1331b8b88baf099a92b6a18ae5e
yontey <muasajohn01@gmail.com>
2019-03-31 19:05:11 +0300

    generated a http service to fetch github data

---
+++ src/app/profile-http/profile-request.service.spec.ts
@@ -0,0 +1,12 @@
+import { TestBed } from '@angular/core/testing';
+
+import { ProfileRequestService } from './profile-request.service';
+
+describe('ProfileRequestService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    const service: ProfileRequestService = TestBed.get(ProfileRequestService);
+    expect(service).toBeTruthy();
+  });
+});
