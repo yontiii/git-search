3876e010cc7b458fe7ab5f00906189f727408d2a
yontey <muasajohn01@gmail.com>
2019-03-31 16:01:55 +0300

    added landing page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import {HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
+import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent
+    UsersComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
